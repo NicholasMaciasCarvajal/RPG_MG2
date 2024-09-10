bcb9318d6ccbd0c0f979f289c974604a941cce70
NicholasMaciasCarvajal <150973370+NicholasMaciasCarvajal@users.noreply.github.com>
2024-09-10 15:42:05 -0600

    Cámara y movimiento del personaje
    Se añadió el script de movimiento y rotación de la cámara, además de algunos modelos

--- Assets/Scripts/MovimientoJugador.cs
+++ Assets/Scripts/MovimientoJugador.cs
@@ -4,5 +4,68 @@ using UnityEngine;
 
 public class MovimientoJugador : MonoBehaviour
 {
-    [SerializedField] private Transform movimiento;
+    /*
+    public float speed = 10.0f;
+    private float translation;
+    private float straffe;
+
+    // Use this for initialization
+    void Start()
+    {
+        // turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxis() is used to get the user's input
+        // You can furthor set it on Unity. (Edit, Project Settings, Input)
+        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+    */
+
+    void Update()
+    {
+        MovimientoCamara();
+        MovimientoPersonaje();
+    }
+
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float velocidadRotación;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Transform transformPersonaje;
+    [SerializeField] private Camera camaraPersonaje;
+
+    private Vector3 movimiento;
+    private float rotacionX;
+
+    void MovimientoPersonaje()
+    {
+        float movX = Input.GetAxis("Horizontal");
+        float movZ = Input.GetAxis("Vertical");
+
+        movimiento = transform.right * movX + transform.forward * movZ;
+        characterController.SimpleMove(movimiento * velocidadMovimiento);
+    }
+
+    void MovimientoCamara()
+    {
+        float ratonX = Input.GetAxis("Mouse X") * velocidadRotación;
+        float ratonY = Input.GetAxis("Mouse Y") * velocidadRotación;
+        
+        rotacionX -= ratonX;
+        rotacionX = Mathf.Clamp(rotacionX, -90f, 90f);
+
+        camaraPersonaje.transform.localRotation = Quaternion.Euler(rotacionX, 0, 0);
+        transformPersonaje.Rotate(Vector3.up * ratonX);
+    }
 }
